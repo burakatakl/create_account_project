9d3f6692383aaf37977f38fc4a19f85d2cf13a29
burakatakl <149960490+burakatakl@users.noreply.github.com>
2024-11-17 16:22:42 +0100

    first I created wrong file after that I fixed

---
+++ app/src/main/java/com/example/create_account_project/CredentialsManager.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+class CredentialsManager {
+
+    private val emailPattern = ("[a-zA-Z0-9\\+\\%\\-\\+]{1,256}" +
+            "\\@" +
+            "[a-zA-Z0-9][0-zA-Z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+            "[a-zA-Z0-9][0-zA-Z0-9\\-]{0,25}" +
+            ")+").toRegex()
+
+    fun isEmailValid(email: String): Boolean {
+        return email.matches(emailPattern)
+    }
+
+    fun isValidPassword(password: String): Boolean {
+        return password.length >= 8
+    }
+
+    fun validateCredentials(email: String, password: String, isCheckboxChecked: Boolean): Boolean {
+        return isEmailValid(email) && isValidPassword(password) && isCheckboxChecked
+    }
+}
