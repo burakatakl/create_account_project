e2b80529ada6b6b87aa7b793c961061a41b8b5a0
burakatakl <149960490+burakatakl@users.noreply.github.com>
2024-11-21 14:19:50 +0100

    updates

--- app/src/test/java/com/example/create_account_project/CredentialsManagerTest.kt
+++ app/src/test/java/com/example/create_account_project/CredentialsManagerTest.kt
@@ -8,22 +8,29 @@ class CredentialsManagerTest {
     private val credentialsManager = CredentialsManager()
 
     @Test
-    fun testValidEmail(){
+    fun testValidEmail() {
         assertTrue(credentialsManager.isEmailValid("example@email.com"))
         assertFalse(credentialsManager.isEmailValid("false_email"))
     }
+
     @Test
-    fun testPassword(){
+    fun testPassword() {
         assertTrue(credentialsManager.isValidPassword("burak123"))
         assertFalse(credentialsManager.isValidPassword("no"))
     }
-    @Test
-    fun testCredentials(){
-        assertTrue(credentialsManager.validateCredentials("burak@test.com","burak123",true))
-        assertFalse(credentialsManager.validateCredentials("false_email","burak123",true))
-        assertFalse(credentialsManager.validateCredentials("burak@test.com","no",true))
 
-        assertFalse(credentialsManager.validateCredentials("burak@test.com","burak123",false))
+    @Test
+    fun testCredentials() {
+        assertTrue(credentialsManager.validateCredentials("burak@test.com", "burak123", true))
+        assertFalse(credentialsManager.validateCredentials("false_email", "burak123", true))
+        assertFalse(credentialsManager.validateCredentials("burak@test.com", "no", true))
+
+        assertFalse(credentialsManager.validateCredentials("burak@test.com", "burak123", false))
+    }
+
+    @Test
+    fun testTermsAccepted() {
+        assertFalse(credentialsManager.termsAccepted(false))
     }
 
 
