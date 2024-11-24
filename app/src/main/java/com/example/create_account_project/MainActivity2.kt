5ca5404e078fe5e761db3861bf75e3337c22a2b9
burakatakl <149960490+burakatakl@users.noreply.github.com>
2024-11-24 17:41:35 +0100

    I changed all code

--- app/src/main/java/com/example/create_account_project/MainActivity2.kt
+++ app/src/main/java/com/example/create_account_project/MainActivity2.kt
@@ -7,40 +7,54 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 
 class MainActivity2 : AppCompatActivity() {
 
-    private lateinit var credentialsManager: CredentialsManager
+    private val credentialsManager: CredentialsManager = CredentialsManager()
+
+    private val fullNameFile: TextInputEditText
+        get() = findViewById(R.id.nameEditText)
+    private val fullNameLayout: TextInputEditText
+        get() = findViewById(R.id.nameInput)
+    private val phoneFile: TextInputEditText
+        get() = findViewById(R.id.phoneNumberEdit)
+    private val phoneLayout: TextInputEditText
+        get() = findViewById(R.id.phoneNumberInput)
+    private val emailFile: TextInputEditText
+        get() = findViewById(R.id.emailEditText)
+    private val emailLayout: TextInputEditText
+        get() = findViewById(R.id.emailInputLayout)
+    private val passwordFile: TextInputEditText
+        get() = findViewById(R.id.passwordEditText)
+    private val passwordLayout: TextInputEditText
+        get() = findViewById(R.id.passwordInputLayout)
+    private val checkBox: CheckBox
+        get() = findViewById(R.id.rememberMeCheckBox)
+    private val nextButton: Button
+        get() = findViewById(R.id.nextButton)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        credentialsManager = CredentialsManager()
 
-        val fullNameEditText = findViewById<EditText>(R.id.nameEditText)
-        val emailEdit = findViewById<EditText>(R.id.emailEditText)
-        val phoneNumberEdit = findViewById<EditText>(R.id.phoneNumberEdit)
-        val passwdEdit = findViewById<EditText>(R.id.passwordEditText)
-        val conditionCheckBox = findViewById<CheckBox>(R.id.rememberMeCheckBox)
-        val nextButton = findViewById<Button>(R.id.nextButton)
-
-        nextButton.setOnClickListener {
-            val fullName = fullNameEditText.text.toString().trim()
-            val email = emailEdit.text.toString().trim()
-            val phoneNumber = phoneNumberEdit.text.toString().trim()
-            val password = passwdEdit.text.toString().trim()
-            val terms = conditionCheckBox.isChecked
-        }
-
-
-        val loginButton = findViewById<View>(R.id.rememberLoginView)
+        nextButton.setOnClickListener { handleNextButtonClick() }
+        val loginButton = findViewById<TextView>(R.id.rememberLoginView)
         loginButton.setOnClickListener {
-            val goToReg = Intent(this, MainActivity::class.java)
-            startActivity(goToReg)
+            val goToCreateAccount = Intent(this, MainActivity::class.java)
+            startActivity(goToCreateAccount)
         }
+    }
+
+    private fun handleNextButtonClick(){
+
     }
 
 
 }
+
+
+
