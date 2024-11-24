ed29a7b4be38af7f880e899825cd491665e92e9a
burakatakl <149960490+burakatakl@users.noreply.github.com>
2024-11-24 16:20:36 +0100

    I changed the code now look much better and also working perfectly

--- app/src/main/java/com/example/create_account_project/MainActivity.kt
+++ app/src/main/java/com/example/create_account_project/MainActivity.kt
@@ -9,34 +9,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import android.content.Intent
 import android.view.View
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private val credentialsManager: CredentialsManager = CredentialsManager()
+    private val emailFile: TextInputEditText
+        get() = findViewById(R.id.emailEditText)
+    private val emailLayout: TextInputEditText
+        get() = findViewById(R.id.emailInputLayout)
+    private val passwordFile: TextInputEditText
+        get() = findViewById(R.id.passwordEditText)
+    private val passwordLayout: TextInputEditText
+        get() = findViewById(R.id.passwordInputLayout)
+    private val nextButton: Button
+        get() = findViewById(R.id.nextButton)
+    private val rememberCheckBox: CheckBox
+        get() = findViewById(R.id.rememberMeCheckBox)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val emailField = findViewById<EditText>(R.id.emailEditText)
-        val passwordField = findViewById<EditText>(R.id.passwordEditText)
-        val nextButton = findViewById<Button>(R.id.nextButton)
-        val CheckedBox = findViewById<CheckBox>(R.id.rememberMeCheckBox)
-
-        nextButton.setOnClickListener {
-            val email = emailField.text.toString().trim()
-            val password = passwordField.text.toString().trim()
-            val rememberMe = CheckedBox.isChecked
-        }
-        val loginButton = findViewById<View>(R.id.registernow)
+        nextButton.setOnClickListener { nextButtonClick() }
+        val loginButton = findViewById<TextView>(R.id.registernow)
         loginButton.setOnClickListener {
             val goToReg = Intent(this, MainActivity2::class.java)
             startActivity(goToReg)
         }
     }
+
     private fun setError(field: EditText, errorResId: Int) {
         field.error = getString(errorResId)
     }
 
-    private fun showToast(messageResId: Int) {
-        Toast.makeText(this, getString(messageResId), Toast.LENGTH_SHORT).show()
+    private fun nextButtonClick() {
+
+
+
     }
 
 }
