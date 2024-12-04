57f90acd75bf042267feff4975812c8167abc196
burakatakl <149960490+burakatakl@users.noreply.github.com>
2024-12-04 17:43:35 +0100

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation("com.google.android.material:material:1.4.0")
-
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.junit.junit)
