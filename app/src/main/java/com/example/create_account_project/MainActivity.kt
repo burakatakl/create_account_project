44c896e7f701a652d612f1b9adbc27edb2e959d7
burakatakl <149960490+burakatakl@users.noreply.github.com>
2024-12-04 22:47:10 +0100

    Creating Fragments

--- app/src/main/java/com/example/create_account_project/MainActivity.kt
+++ app/src/main/java/com/example/create_account_project/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
